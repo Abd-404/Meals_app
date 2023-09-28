9edfad087189b54716897c79a4c1ebfd7254e177
Abd-404 <abdullahmeraj243@gmail.com>
2023-09-28 22:15:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Meals App
+
+The Meals App is your ultimate culinary companion, offering a variety of delicious recipes at your fingertips. Explore diverse meal categories, discover mouthwatering dishes, and cook like a pro. With filters to cater to dietary preferences and a favorites feature, meal planning has never been easier.
+
+# Screenshots
+
+![Alt text](assets/meals.gif)
+
+<hr>
+
+![Alt text](assets/favourites.gif)
+
+<hr>
+
+![Alt text](assets/filters.gif)
+
+# Features
+
+- **Categories Screen**: Browse through ten tempting meal categories displayed in a grid layout. Select a category to explore the meals it offers.
+
+- **Meals Screen**: Find detailed meal listings, complete with images, cooking time, affordability, and difficulty level. Tap on a meal to view its ingredients and step-by-step instructions.
+
+- **Favorites**: Mark your favorite meals with a simple tap on the heart icon, and access them conveniently in the "Favorites" tab.
+
+- **Filters**: Customize your meal search by applying filters such as Gluten-free, Lactose-free, Vegetarian, and Vegan.
+
+- **Side Drawer**: Access filters settings from the side drawer for a tailored culinary experience.
+
+# Installation
+
+To try out the Meals App, follow these steps:
+
+1. **Clone the repository to your local machine:**
+
+```
+git clone https://github.com/Abd-404/Meals_app.git
+```
+
+2. **Navigate to the project directory:**
+
+```
+cd Meals_app
+```
+
+3. **Install dependencies:**
+
+```
+flutter pub get
+```
+
+4. **Run the app on your preferred device:**
+
+```
+flutter run
+```
---
+++ lib/screens/tabs.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+// import 'package:meals_app/data/dummy_data.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/filters.dart';
+import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
+import 'package:meals_app/providers/favourites_provider.dart';
+import 'package:meals_app/providers/filters_provider.dart';
+
+class TabsScreen extends ConsumerStatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  ConsumerState<TabsScreen> createState() => _TabsScreen();
+}
+
+class _TabsScreen extends ConsumerState<TabsScreen> {
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  void _setScreen(String identifier) async {
+    Navigator.of(context).pop();
+    if (identifier == 'filters') {
+      Navigator.of(context).push(
+        MaterialPageRoute(builder: (ctx) => const FiltersScreen()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final availableMeals = ref.watch(filteredMealsProvider);
+    Widget activePage = CategoriesScreen(
+      availableMeals: availableMeals,
+    );
+    var activePageTitle = 'Categories';
+
+    if (_selectedPageIndex == 1) {
+      final favouriteMeals = ref.watch(favouriteMealsProvider);
+      activePage = MealsScreen(
+        meals: favouriteMeals,
+      );
+      activePageTitle = 'Your Favourites';
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activePageTitle),
+      ),
+      drawer: MainDrawer(
+        onSelectScreen: _setScreen,
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedPageIndex,
+        onTap: _selectPage,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.set_meal), label: 'Categories'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Faviourites'),
+        ],
+      ),
+    );
+  }
+}
